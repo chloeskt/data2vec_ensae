02f96fe1df844f6c8134f1387b9ac82d2e342981
chloeskt <chloe.sekkat@ensae.fr>
2022-04-08 13:40:28 +0200

    question_answering: introducing noisifer

--- source/question_answering/question_answering/__init__.py
+++ source/question_answering/question_answering/__init__.py
@@ -3,3 +3,4 @@ from .models import Data2VecQA
 from .processing import Preprocessor, QADataset
 from .trainers import TrainerArguments, DataArguments, TokenBasedModelTrainer, CharacterBasedModelTrainer
 from .utils import to_pandas, set_seed, remove_examples_longer_than_threshold
+from .noisifier import NoisifierArguments, Noisifier
